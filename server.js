8e7223380ee7430b43ab729aa7014555de554331
UncleMle <62445503+UncleMle@users.noreply.github.com>
2023-05-23 19:37:52 +0100

    Update server.js
    Updated server side with method to stop carrying player.

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 mp.events.add({
-    'player:carry': async(player, target) => {
+    'player:carry': async (player, target) => {
         player.setVariable('carryInfo', target)
-      },
+    },
+    'player:stopCarry': async (player) => {
+        if (player.getVariable('carryInfo')) {
+            player.call('detachAll', [player.getVariable('carryInfo')]);
+            player.setVariable('carryInfo', null)
+            return;
+        }
+    },
 })
