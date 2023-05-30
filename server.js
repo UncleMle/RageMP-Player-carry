a0099ba6c62f036f09f760f565020a800cdb3ad1
UncleMle <62445503+UncleMle@users.noreply.github.com>
2023-05-30 02:06:12 +0100

    Update server.js
    Server 'dropAnim' datahandler to sync entity task clear.

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ mp.events.add({
     },
     'player:stopCarry': (player) => {
         if (player.getVariable('carryInfo')) {
-            player.call('detachAll', [player.getVariable('carryInfo')]);
+            var target = player.getVariable('carryInfo')
+            player.call('detachAll', [target]);
+            target.data.dropAnim = true
             player.setVariable('carryInfo', null)
             return;
         }
